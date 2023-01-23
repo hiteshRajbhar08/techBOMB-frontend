e9787e4cbbca8c7e81a6b99408c53dc2e3ea12e5
Hitesh <h08rajbhar@gmail.com>
2023-01-23 16:48:49 +0530

    cart functionality

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,4 +1,36 @@
+import { useEffect, useState } from 'react';
+import { Link, useLocation, useParams, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+  ListGroupItem,
+} from 'react-bootstrap';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+import { addToCart } from '../redux/actions/cartAction';
+
 const CartPage = () => {
+  const { id: productId } = useParams();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const qty = location.search ? Number(location.search.split('=')[1]) : 1;
+
+  const { cartItems } = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
+
   return <div>CartPage</div>;
 };
 
